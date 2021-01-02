a6f5b377dab8daad07d7b3a0ad994db1f7dfa31c
irrem <50523883+irrem@users.noreply.github.com>
2021-01-02 16:23:48 +0300

    created a google map activity

---
+++ app/src/main/java/com/example/travel_book/MapsActivity.java
@@ -0,0 +1,39 @@
+package com.example.travel_book;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng antkabir = new LatLng(39.92528435685113, 32.836986812500925);
+        mMap.addMarker(new MarkerOptions().position(antkabir).title("Marker in Anıtkabir"));
+       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antkabir,12));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(antkabir));
+    }
+}

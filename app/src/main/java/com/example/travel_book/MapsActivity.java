4bb3ddc9e568db90171ca7e61d5c4b2352e01ab6
irrem <50523883+irrem@users.noreply.github.com>
2021-01-02 23:37:25 +0300

    get last position

--- app/src/main/java/com/example/travel_book/MapsActivity.java
+++ app/src/main/java/com/example/travel_book/MapsActivity.java
@@ -1,7 +1,16 @@
 package com.example.travel_book;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,9 +21,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.Permission;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationListener locationListener;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +42,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        locationManager= (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener=new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                mMap.clear();
+                LatLng userLocation= new LatLng(location.getLatitude(),location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(userLocation).title("You are here"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,14));
 
-        // Add a marker in Sydney and move the camera
-        LatLng antkabir = new LatLng(39.92528435685113, 32.836986812500925);
-        mMap.addMarker(new MarkerOptions().position(antkabir).title("Marker in Anıtkabir"));
-       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antkabir,12));
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(antkabir));
+                System.out.println("location"+location.toString() );
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},1);
+            //permission
+        }
+        else {
+            /* location */
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                LatLng userLastLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(userLastLocation).title("You was here"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLastLocation,14));
+            }
+        }
+
+      // LatLng antkabir = new LatLng(39.92528435685113, 32.836986812500925);
+      // mMap.addMarker(new MarkerOptions().position(antkabir).title("Marker in Anıtkabir"));
+      //  mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antkabir,12));
+      // mMap.moveCamera(CameraUpdateFactory.newLatLng(antkabir));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(grantResults.length>0){
+            if(requestCode==1){
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
